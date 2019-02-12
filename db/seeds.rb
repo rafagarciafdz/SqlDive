11c6f7198f10fdfd1b857f42de95a9002ebdb160
Rafael Garcia <rafagarciafdz@gmail.com>
2019-02-12 16:12:44 -0600

    New project demonstrated the game of active record

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+vader = Author.create!(name: "Vader", country: "Mustafar")
+luke  = Author.create!(name: "Luke", country: "Tatooine")
+leia  = Author.create!(name: "Leia", country: "Alderaan")
+
+fiction     = Genre.create!(name: "Fiction")
+non_fiction = Genre.create!(name: "Non-Fiction")
+biographies = Genre.create!(name: "Biographies")
+
+Book.create!(title: "The Force", genre_id: non_fiction.id, author_id: luke.id, sales: 500)
+Book.create!(title: "Britney Speares: An Anthology", genre_id: biographies.id, author_id: vader.id, sales: 950)
+Book.create!(title: "Only One Direction", genre_id: biographies.id, author_id: vader.id, sales: 45)
+Book.create!(title: "DIY Deathstar", genre_id: non_fiction.id, author_id: vader.id, sales: 1200)
